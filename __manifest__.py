8d054f4fcce8a703adb1300f1f41b43883d65df4
antdesinger <kingbox@88v88v.com>
2019-06-16 22:08:17 +0800

    添加了模块logo

--- __manifest__.py
+++ __manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "AccountCore",
-
     'summary': """
        一个独立的中国会计模块""",
-
     'description': """
         和odoo自带的会计模块完全无关。以简单、直接、易用、智能为设计原则。没有繁琐的初始化和配置，不用结账和反结账、过账等冗余操作是中国会计的最佳实践。
     """,
-    'author': "huangTiger",
-    'website': "http://www.88v88v.com",
+    'author': "黄虎",
+    'website': "http://www.baidu.com",
     'category': 'Uncategorized',
     'version': '0.1',
     'depends': ['base'],
+    'application': True,
+    'images': ['static/description/icon.png'],
     'data': [
         'security/users.xml',
         'views/views.xml',
